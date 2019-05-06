fd5946a6b33f38dfb5c244fb7b3ead35c7c75a9d
maochaowu <maochaowu@ebikj.com>
2019-05-06 22:26:18 +0800

    new project

---
+++ src/main/java/com/mcw/orm/App.java
@@ -0,0 +1,13 @@
+package com.mcw.orm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
